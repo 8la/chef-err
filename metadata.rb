88942db801626a89f73f454a6bfa816a5ca161e0
Damian Pantano <damian@needle.com>
2015-06-10 10:34:45 -0600

    Bumping metadata to 1.0.0 for merge to master

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@needle.com'
 license          'Apache 2.0'
 description      'Installs/Configures err pluggable chat bot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '1.0.0'
 
 depends 'git'
 depends 'python'
